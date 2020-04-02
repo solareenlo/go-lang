835922196b67a40e062cc4403b561c8409c4ddd2
solareenlo <solareenlo@gmail.com>
2020-04-02 19:35:27 +0900

    update basic

---
+++ 01_basic/Short-Variable-Declarations.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+  var i, j int = 1, 2
+  k := 3
+  c, cpp, rust := true, true, "yes"
+  fmt.Println(i, j, k, c, cpp, rust)
+}
---
+++ 01_basic/Variables.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+var c, cpp, rust bool
+
+func main() {
+  var i int
+  fmt.Println(c, cpp, rust, i)
+}
